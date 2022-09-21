f5316493e5fa8aca85e7cfd6d74da725bd0a9abf
Eduardo Rodrigues Santana <37391782+SantanaC4@users.noreply.github.com>
2022-09-21 13:06:31 -0300

    Webpack bundling app code

--- src/get-user.js
+++ src/get-user.js
@@ -1,11 +1,6 @@
-const meuCabecalho = document.getElementById('initial-text');
-meuCabecalho.textContent = 'Github Profile Search';
+import { putRepo } from "./put-repo";
 
-const meuBotao = document.querySelector('button');
-meuBotao.addEventListener("click", getUser);
-
-
-function getUser() {  
+export function getUser() {  
     document.getElementById('list-rep').innerHTML = '';
     var gitUser = document.querySelector('input')
     fetch(`https://api.github.com/users/${gitUser.value}`)  //Search for github user 
@@ -34,16 +29,3 @@ function getUser() {
       console.log(error)
     });
 }
-
-function putRepo(data) {
-  var listDiv = document.getElementById("list-rep");
-  var ul=document.createElement('ul');
-  for (var i = 0; i < data.length; ++i) {
-    var li=document.createElement('li');
-    li.innerHTML = data[i].name;
-    ul.appendChild(li);
-  }
-  listDiv.appendChild(ul);
-}
-var myImage = document.querySelector('img');
-//test comment 
