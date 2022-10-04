9e38472261a742b81333f40243f59bf0460f2168
Eduardo Rodrigues Santana <37391782+SantanaC4@users.noreply.github.com>
2022-10-04 08:20:31 -0300

    Working in github user form

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,10 +1,13 @@
 import Todos from './components/Todos';
+import Todo from './models/todo'
 import GetGihubUser from './components/GetGithubUser';
 
 function App() {
+  const todos = [new Todo('Learn React'), new Todo('Learn TypeScript')];
+
   return (
     <div>
-      <Todos />
+      <Todos items={todos}/>
       <h1>Github Profile Search</h1>
       <GetGihubUser />
     </div>
--- react-ts/src/components/GetGithubUser.tsx
+++ react-ts/src/components/GetGithubUser.tsx
@@ -1,7 +1,27 @@
-function GetGithubUser() {
+import React from "react";
+import { useRef } from "react"
+
+const GetGithubUser = () => {
+    const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+    const submitHandler = (event: React.FormEvent) => {
+        event.preventDefault();
+
+        const enteredText = todoTextInputRef.current!.value;
+
+        if (enteredText.trim().length === 0) {
+            // throw an error
+            return;
+        }
+    };
+
     return (
-        <input type="text" placeholder="Search.."></input>
-    ) 
-}
+        <form onSubmit={submitHandler}>
+        <label htmlFor="text">Github Profile Search</label>
+        <input type="text" placeholder="Type.." id="searchText" ref={todoTextInputRef}/>
+        <button>Search</button>
+        </form>
+    ); 
+};
 
 export default GetGithubUser; 
