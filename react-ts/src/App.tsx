e9a9a7e87f94462b2d7bd761ea14451b88d1a00f
Eduardo Rodrigues Santana <37391782+SantanaC4@users.noreply.github.com>
2022-10-13 15:50:04 -0300

    Axios call works

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,15 +1,29 @@
-import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
+import axios from "axios"
 import Todos from './components/Todos';
+//import Routes from './Routes';
 import Todo from './models/todo'
 import GetGihubUser from './components/GetGithubUser';
-import AxiosInReact from './components/AxiosInReact';
+//import AxiosInReact from './components/AxiosInReact';
 
 
-const URL = "https://jsonplaceholder.typicode.com/posts/1";
+//const URL = "https://jsonplaceholder.typicode.com/posts/1";
 
 function App() {
   //const todos = [new Todo('Learn React'), new Todo('Learn TypeScript')];
+
+  const [people, setpeople] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://api.github.com/users/santanac4/repos').then((response) => {
+      console.log(response);
+
+      setpeople(response?.data)
+      debugger
+    })
+  },[]);
+
+
   const [todos, setTodos] = useState<Todo[]>([]);
 
   const addTodoHandler = (todoText: string) => {
@@ -25,7 +39,16 @@ function App() {
       <GetGihubUser onAddTodo={addTodoHandler}/>
       <Todos items={todos}/>
       <h1>Github Profile Search</h1>
-      <AxiosInReact baseURL={URL}/>
+      {people.length 
+        ? ( 
+            <>
+                {people.map((person: any) => (
+                        <h1>{person.name}</h1>
+                ))}
+            </>
+        )
+        : <div>Loading..</div>}
+
     </div>
   );
 }
