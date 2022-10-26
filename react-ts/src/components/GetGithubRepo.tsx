02d2bd9150448ce8c295d2501c589461f80b1e70
Eduardo Rodrigues Santana <37391782+SantanaC4@users.noreply.github.com>
2022-10-26 17:18:53 -0300

    Handling error from the axios call

--- react-ts/src/components/GetGithubRepo.tsx
+++ react-ts/src/components/GetGithubRepo.tsx
@@ -1,37 +1,35 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import React from 'react'
 import { APIResponse, Repository } from '../models/types'
 import { get } from '../fetchers/Fetchers'
 import PutRepositories from "./PutRepositories";
 
-const baseURL = 'https://api.github.com/users/';
 
 const GetGithubRepo: React.FC<{userName: string}> = (props) => {
     const [data, setData] = useState<Repository[]>([]);
     const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
+    const [error, setError] = useState('');
 
-    async function getData() {
+    const getData = useCallback(async () => {
         setIsLoading(true);
-        setError(null);
-        try {
-            const results = await get<APIResponse>(`${baseURL}${props.userName}/repos`);
-            setData(results);
-        } catch (error) {
-            console.error(error);
-        }
-    }
+        setError('');
+        const results = await get<APIResponse>(`${props.userName}/reos`);
+        if (typeof (results) === 'string') {
+            setError(results);
+        } else setData(results);
+    },[props.userName]);
 
     useEffect(() => {
         getData();
         setIsLoading(false);
-    },[props.userName]);
+    },[getData]);
 
     return (
         <React.Fragment>
         <section>
             {!isLoading && data.length > 0 && <PutRepositories items={data}/>}
             {!isLoading && data.length === 0 && <p>Found no Repositories</p>}
+            {!isLoading && error.length && <p>{error}</p>}
             {isLoading && <p>Loading...</p>}
         </section>
         </React.Fragment>
--- react-ts/src/fetchers/Fetchers.tsx
+++ react-ts/src/fetchers/Fetchers.tsx
@@ -1,9 +1,22 @@
 import axios from 'axios'
 
+const baseURL = 'https://api.github.com/users/';
+
 export async function get<T>(
     path: string
-): Promise<T> {
-    const { data } = await axios.get(path);
-    console.log(data);
-    return data;
+): Promise<T | string> {
+    try {
+        const { data, status } = await axios.get(`${baseURL}${path}`);
+
+        console.log('Response status is: ', status);
+        return data;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            
+            console.log('Error message:', error.message);
+            return error.message;
+        } else {
+            return 'An unexpectted error occurred';
+        }
+    }
 };
